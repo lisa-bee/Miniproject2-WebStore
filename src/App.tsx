f9f102a000052396fcb5cb014f3ccc7dfee7fef9
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-03-22 18:44:02 +0100

    Added hero to startpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import { Grommet } from "grommet";
 import ProductPage from "./ProductPage";
 import Header1 from "./Header";
+import StartPage from "./StartPage";
 
 function App() {
   return (
     <div className="App">
       <Header1 />
+      <StartPage />
       <ProductPage />
     </div>
   );
