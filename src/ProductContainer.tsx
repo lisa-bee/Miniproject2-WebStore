4744f6c8da745a8783b4fb025337743408ef70db
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-03-23 17:24:48 +0100

    Added title and price hardcoded

--- src/ProductContainer.tsx
+++ src/ProductContainer.tsx
@@ -7,10 +7,20 @@ export default class ProductContainer extends React.Component<{}, {}> {
     let image = require("./assets/image1.jpg");
     return (
       <Grommet theme={grommet}>
-        <Box margin="small" width="small" height="small">
-          <Image fit="cover" src={image} />
+        <Box align="center">
+          <Box margin="small" width="small" height="small">
+            <Image fit="cover" src={image} />
+          </Box>
+          <Box align="center">
+            <p style={noMargin()}>Title</p>
+            <p style={noMargin()}>249 SEK</p>
+          </Box>
         </Box>
       </Grommet>
     );
   }
 }
+
+const noMargin = (): CSSProperties => ({
+  margin: 0
+});
