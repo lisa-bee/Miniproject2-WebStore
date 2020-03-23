083d7d8d344bdce8801d69c63e07fe870675cfdc
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-03-23 12:12:45 +0100

    Fixed a bit in header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Grommet, Header, Image } from "grommet";
+import React, { CSSProperties } from "react";
+import { Box, Grommet, Header, Image, Button } from "grommet";
 import { grommet } from "grommet/themes";
 import { Cart } from "grommet-icons";
 
@@ -8,21 +8,48 @@ export default class Header1 extends React.Component {
     let image = require("./assets/logo.png");
     return (
       <Grommet theme={grommet}>
-        <Header background="light-1" pad="medium">
+        <Header justify="between" background="light-1" pad="medium">
           <Box>
             <Image fit="contain" src={image} />
           </Box>
-          <Box direction="row" gap="medium">
+          <Box direction="row" align="center">
+            <div style={shoppingCountContainer()}>
+              <p style={shoppingCount()}>0</p>
+            </div>
             <Cart
               onClick={() => {
                 alert("Shopping cart");
               }}
               color="plain"
-              size="large"
+              size="medium"
             />
+            <Button
+              margin="small"
+              primary
+              color="dark-1"
+              label="Go to check out"
+              onClick={() => {
+                alert("Shopping cart");
+              }}
+            ></Button>
           </Box>
         </Header>
       </Grommet>
     );
   }
 }
+
+const shoppingCountContainer = (): CSSProperties => ({
+  height: "1.4rem",
+  width: "1.4rem",
+  backgroundColor: "#3D138D",
+  borderRadius: "2rem"
+});
+
+const shoppingCount = (): CSSProperties => ({
+  position: "absolute",
+  top: "2.8rem",
+  right: "16.37rem",
+  color: "white",
+  fontSize: "0.8rem"
+});
