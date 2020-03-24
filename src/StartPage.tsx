9b3861d6f5ce496715a5d6c428f993234bad366d
Markus Karlsson <markuskarlsson__@hotmail.com>
2020-03-24 10:09:41 +0100

    Fixed startpage

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Hero from "./Hero";
-import { Box, Grommet, Button } from "grommet";
-import { grommet } from "grommet/themes";
+import { Box, Button } from "grommet";
 import { Link } from "react-router-dom";
 
 export default class StartPage extends React.Component {
   render() {
     return (
-      <Grommet theme={grommet}>
-        <Box direction="row-responsive" justify="center" align="center">
-          <Hero />
-        </Box>
-        <Link to="/productpage/">
-          <Button></Button>
-        </Link>
-      </Grommet>
+        <>
+          <Box direction="row-responsive" justify="center" align="center">
+            <Hero />
+          </Box>
+          <Link to="/productpage/">
+            <Button></Button>
+          </Link>
+        </>
     );
   }
 }
