3cf84e0739cf70c23801b9f222ab335a083a3fd1
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-03-29 17:06:23 +0200

    Fixed some responsiveness

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties } from "react";
-import { Box, Header, Image, Button } from "grommet";
+import { Box, Header, Image, Button, ResponsiveContext } from "grommet";
 import { Cart } from "grommet-icons";
 import { Link } from "react-router-dom";
 import { CartConsumer } from "../contexts/CartContext";
@@ -10,28 +10,38 @@ export default class Header1 extends React.Component {
     return (
       <CartConsumer>
         {({ items, addProductToCart }) => (
-          <Header justify="between" background="light-1" pad="small">
-            <Box>
-              <Link to="/">
-                <Image fit="contain" src={image} />
-              </Link>
-            </Box>
-            <Link to="/checkoutpage">
-              <Box direction="row" align="center">
-                <div style={shoppingCountContainer()}>
-                  <p style={shoppingCount()}>{items.length}</p>
-                </div>
-                <Cart color="plain" size="medium" />
-                <Button
-                  primary
-                  margin="small"
-                  color="dark-1"
-                  label="Go to check out"
-                  //onClick={() => addProductToCart(product)}
-                ></Button>
-              </Box>
-            </Link>
-          </Header>
+          <ResponsiveContext.Consumer>
+            {size => (
+              <Header justify="between" background="light-1" pad="small">
+                <Box margin={{ left: "large" }}>
+                  <Link to="/">
+                    <Image fit="contain" src={image} />
+                  </Link>
+                </Box>
+                <Link to="/checkoutpage">
+                  <Box
+                    direction="row"
+                    align="center"
+                    margin={{ right: "large" }}
+                  >
+                    <div style={shoppingCountContainer()}>
+                      <p style={shoppingCount()}>{items.length}</p>
+                    </div>
+                    <Cart color="plain" size="medium" />
+                    {size != "small" && (
+                      <Button
+                        primary
+                        margin="small"
+                        color="dark-1"
+                        label="Go to check out"
+                        //onClick={() => addProductToCart(product)}
+                      ></Button>
+                    )}
+                  </Box>
+                </Link>
+              </Header>
+            )}
+          </ResponsiveContext.Consumer>
         )}
       </CartConsumer>
     );
@@ -42,13 +52,13 @@ const shoppingCountContainer = (): CSSProperties => ({
   height: "1.4rem",
   width: "1.4rem",
   backgroundColor: "#3D138D",
-  borderRadius: "2rem"
+  borderRadius: "2rem",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
 });
 
 const shoppingCount = (): CSSProperties => ({
-  position: "absolute",
-  top: "2.23rem",
-  right: "15.58rem",
   color: "white",
   fontSize: "0.8rem"
 });
--- src/Components/StartPage.tsx
+++ src/Components/StartPage.tsx
@@ -11,6 +11,24 @@ export default class StartPage extends React.Component {
         <Box direction="row-responsive" justify="center" align="center">
           <Hero />
         </Box>
+        <Box
+          width="100%"
+          justify="center"
+          align="center"
+          margin={{ top: "medium" }}
+          style={{ textAlign: "center" }}
+        >
+          <Box width="large" pad="medium">
+            <p>
+              Discover the latest trends in posters online. We have a wide
+              selection of attractive posters online and launch new designs
+              every week. In our magazines you will find inspiration on how to
+              decorate your home nicely together with our posters and paintings.
+              With us you can buy beautiful posters and paintings online and we
+              always offer fast deliveries and low prices.
+            </p>
+          </Box>
+        </Box>
         <AllProducts />
         <Link to="/productpage/">
           <Button></Button>
