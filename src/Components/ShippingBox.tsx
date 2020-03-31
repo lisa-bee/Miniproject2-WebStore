3d1dcbc355dcec44d45ccb9f8af4abc19d0a6747
Lisa <lisa.bengtsson@live.com>
2020-03-31 16:41:47 +0200

    get a different delivery date with each option

--- src/Components/ShippingBox.tsx
+++ src/Components/ShippingBox.tsx
@@ -1,33 +1,72 @@
 import React from "react";
-import { Box, Text, RadioButtonGroup } from "grommet";
-import { Deliver } from "grommet-icons";
-
+import { Box, Text, RadioButtonGroup, RadioButton } from "grommet";
+import { Deliver, Target } from "grommet-icons";
 
 interface Props {}
 
 interface State {}
 
 export default class ShippingBox extends React.Component<Props, State> {
-  
-  
-  calculateDeliveryDate() {
+  postNord() {
     const date = new Date();
-    const newDate = new Date(date);
-    newDate.setDate(newDate.getDate()+ 3);
-    return newDate.toDateString();
+
+    const postNord = new Date(date);
+    postNord.setDate(postNord.getDate() + 1);
+
+    return postNord.toDateString();
   }
 
+  DHL() {
+    const date = new Date();
+
+    const DHL = new Date(date);
+    DHL.setDate(DHL.getDate() + 3);
+
+    return DHL.toDateString();
+  }
+
+  Schenker() {
+    const date = new Date();
+
+    const Schenker = new Date(date);
+    Schenker.setDate(Schenker.getDate() + 5);
+
+    return Schenker.toDateString();
+  }
 
   render() {
     return (
-        <Box pad="xlarge" gap="large" width="large" background="light-1">
-          <Box width="large" direction="row" justify="between">
-            <Text size="large" alignSelf="start" weight="bold">
-              Shipping
-            </Text>
-            <Deliver color="brand"></Deliver>
-          </Box>
-        <RadioButtonGroup
+      <Box pad="xlarge" gap="large" width="large" background="light-1">
+        <Box width="large" direction="row" justify="between">
+          <Text size="large" alignSelf="start" weight="bold">
+            Shipping
+          </Text>
+          <Deliver color="brand"></Deliver>
+        </Box>
+        <Box gap="medium">
+          <RadioButton
+            name="portNord"
+            label="PostNord (1 day) + 49 SEK"
+          />
+          <Text style={{ fontSize: "12pt", paddingLeft: "1.9rem" }}>
+            Receive your order on <strong>{this.postNord()}</strong>
+          </Text>
+          <RadioButton
+            name="DHL"
+            label="DHL (3 days) + 29 SEK"
+          />
+          <Text style={{ fontSize: "12pt", paddingLeft: "1.9rem" }}>
+            Receive your order on <strong>{this.DHL()}</strong>
+          </Text>
+          <RadioButton
+            name="Schenker"
+            label="Schenker(5 days) + 0 SEK"
+          />
+          <Text style={{ fontSize: "12pt", paddingLeft: "1.9rem" }}>
+            Receive your order on <strong>{this.Schenker()}</strong>
+          </Text>
+        </Box>
+        {/*  <RadioButtonGroup
           name="deliveryOptions"
           // onChange= "target": { value } }) => {...}
           options={[
@@ -50,9 +89,7 @@ export default class ShippingBox extends React.Component<Props, State> {
               label: "Schenker (3 days) + 29 SEK"
             }
           ]}
-        />
-       <Text>You will receive your order on {this.calculateDeliveryDate()}</Text>
-       
+        /> */}
         {/* <Button
             type="submit"
             alignSelf="center"
