98a49a26d8e728c894b45babd54f4aa078e5aaa9
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-04-02 19:01:58 +0200

    In progress

--- src/Components/ConfirmationPopup.tsx
+++ src/Components/ConfirmationPopup.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties } from "react";
-import { Box, Button } from "grommet";
+import { Box, Button, ResponsiveContext } from "grommet";
 import { Link } from "react-router-dom";
 
 interface Props {
@@ -10,39 +10,41 @@ interface State {}
 export default class ConfirmationPopup extends React.Component<Props, State> {
   render() {
     return (
-      <>
-        <Box
-          background="light-4"
-          height="medium"
-          className="cartMessage"
-          style={cartMessage}
-        >
-          <Box pad="small">
-          <h1>One item added to cart!</h1>
-          <p>Continue shopping or go to cart?</p>
-          </Box>
-          <Box direction="row-responsive">
-            <Box direction="row">
-            <Button
-              onClick={this.props.closeDiv}
-              margin="small"
-              label="Continue"
-              primary
-              color="dark-1"
-            ></Button>
-            <Link to="/checkoutpage/">
-              <Button
-                onClick={this.props.closeDiv}
-                margin="small"
-                label="Go to cart"
-                primary
-                color="dark-1"
-              ></Button>
-            </Link>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box
+            background="light-4"
+            height="medium"
+            className="cartMessage"
+            style={cartMessage}
+          >
+            <Box pad="small">
+              <h1>One item added to cart!</h1>
+              <p>Continue shopping or go to cart?</p>
+            </Box>
+            <Box direction="row-responsive">
+              <Box direction="row">
+                <Button
+                  onClick={this.props.closeDiv}
+                  margin="small"
+                  label="Continue"
+                  primary
+                  color="dark-1"
+                ></Button>
+                <Link to="/checkoutpage/">
+                  <Button
+                    onClick={this.props.closeDiv}
+                    margin="small"
+                    label="Go to cart"
+                    primary
+                    color="dark-1"
+                  ></Button>
+                </Link>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </>
+        )}
+      </ResponsiveContext.Consumer>
     );
   }
 }
