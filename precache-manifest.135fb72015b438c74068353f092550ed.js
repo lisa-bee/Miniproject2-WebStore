148fcc2a153326c85e4a190dac021cd8edc981f5
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-04-03 14:07:42 +0200

    Updates

---
+++ precache-manifest.135fb72015b438c74068353f092550ed.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6948f45998295a67952e8e0af20c1e87",
+    "url": "/Miniproject2-WebStore/index.html"
+  },
+  {
+    "revision": "5cf61c50279d0992531c",
+    "url": "/Miniproject2-WebStore/static/css/main.ec1e4351.chunk.css"
+  },
+  {
+    "revision": "27a6628f67749aacf543",
+    "url": "/Miniproject2-WebStore/static/js/2.3bf7cfae.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Miniproject2-WebStore/static/js/2.3bf7cfae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf61c50279d0992531c",
+    "url": "/Miniproject2-WebStore/static/js/main.0a7cbc99.chunk.js"
+  },
+  {
+    "revision": "ef9ab9ae9508f5a6c183",
+    "url": "/Miniproject2-WebStore/static/js/runtime-main.35bd99fa.js"
+  },
+  {
+    "revision": "57d7985b91fbfee2e286b91fbfc5f624",
+    "url": "/Miniproject2-WebStore/static/media/ape.57d7985b.jpg"
+  },
+  {
+    "revision": "ba73ca67e8b1f171608a2de71a387733",
+    "url": "/Miniproject2-WebStore/static/media/brownie.ba73ca67.jpg"
+  },
+  {
+    "revision": "38cee52a4947726c86f931b3e9d1348f",
+    "url": "/Miniproject2-WebStore/static/media/cactus.38cee52a.jpg"
+  },
+  {
+    "revision": "0c5eb4c38f30af083349bdac14111faa",
+    "url": "/Miniproject2-WebStore/static/media/coffe1.0c5eb4c3.jpg"
+  },
+  {
+    "revision": "82160599e55ea8637480246061efb320",
+    "url": "/Miniproject2-WebStore/static/media/gold1.82160599.jpg"
+  },
+  {
+    "revision": "cbf50a731b3791d76e66a7460c65f72a",
+    "url": "/Miniproject2-WebStore/static/media/gold2.cbf50a73.jpg"
+  },
+  {
+    "revision": "f9995754b8ea9631db9aeb4b4da105a6",
+    "url": "/Miniproject2-WebStore/static/media/gold3.f9995754.jpg"
+  },
+  {
+    "revision": "faa0e5b3ce5a6f3eef163aaff4db3426",
+    "url": "/Miniproject2-WebStore/static/media/livingroom.faa0e5b3.mp4"
+  },
+  {
+    "revision": "d4e2166a20b7f57cd8c263f845a8609c",
+    "url": "/Miniproject2-WebStore/static/media/marra.d4e2166a.jpg"
+  },
+  {
+    "revision": "17473160b52c4b1673f36ca241c79439",
+    "url": "/Miniproject2-WebStore/static/media/marra1.17473160.jpg"
+  },
+  {
+    "revision": "96649812779d7469e2e649eca4ff89e8",
+    "url": "/Miniproject2-WebStore/static/media/wood.96649812.jpg"
+  }
+]);
